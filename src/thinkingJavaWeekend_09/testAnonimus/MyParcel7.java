28d0c762c5b13b5b2d33e78c85249737f183f74c
ckatt <ckatt@ukr.net>
2019-11-07 15:32:17 +0200

    anonumus vs inner classes

---
+++ src/thinkingJavaWeekend_09/testAnonimus/MyParcel7.java
@@ -0,0 +1,14 @@
+package thinkingJavaWeekend_09.testAnonimus;
+
+public class MyParcel7 {
+
+	public MyParcel7() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/thinkingJavaWeekend_09/testAnonimus/MyParcel9.java
@@ -0,0 +1,60 @@
+package thinkingJavaWeekend_09.testAnonimus;
+
+import myClass.Ckatt;
+import thinkingJavaWeekend_09.localInnerClass.Parcel5;
+import thinkingJavaWeekend_09.upwardConversionInnerClass.Destination;
+
+public class MyParcel9 {
+
+	public MyParcel9() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/*public Destination destination(String s) {
+		class PDestination implements Destination {
+			private String label;
+
+			private PDestination(String whereTo) {
+				// TODO Auto-generated constructor stub
+
+				label = whereTo;
+
+			}
+
+			@Override
+			public String readlabel() {
+				// TODO Auto-generated method stub
+				return label;
+			}
+		}
+
+		return new PDestination(s);
+	}*/
+
+	public Destination destinationAnonymous(String s) {
+		return new Destination() {
+			
+			private String label = s;
+			
+			@Override
+			public String readlabel() {
+				// TODO Auto-generated method stub
+				return label;
+			}
+			
+		};
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MyParcel9 p = new MyParcel9();
+		Destination d = p.destinationAnonymous("Танзания");
+        Ckatt.outConsole(d.toString());
+	}
+	
+	
+}
