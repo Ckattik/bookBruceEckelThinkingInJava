bfede12d581571370309c22f2c80de48dbd42e4a
ckatt <ckatt@ukr.net>
2019-11-07 13:59:16 +0200

    anonimus class

---
+++ src/thinkingJavaWeekend_09/anonimusInnerClass/Parcel7.java
@@ -0,0 +1,32 @@
+package thinkingJavaWeekend_09.anonimusInnerClass;
+
+import thinkingJavaWeekend_09.upwardConversionInnerClass.Contents;
+
+public class Parcel7 {
+
+	public Parcel7() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Contents contents() {
+		
+		return new Contents() { //вставка анонимного класса 
+			private int i = 11;
+			public int value() {
+				return i;
+			}
+			
+		};
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Parcel7 p = new Parcel7();
+		Contents c = p.contents();
+		
+	}
+
+}
