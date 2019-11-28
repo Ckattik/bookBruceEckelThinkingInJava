0c025493e5b644f1abf23f09262c2dbf5f36fcb0
ckatt <ckatt@ukr.net>
2019-11-28 08:11:45 +0200

    change Adding group

--- src/thinkinginJavaWeekend_11/holding/AddingGroups.java
+++ src/thinkinginJavaWeekend_11/holding/AddingGroups.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import myClass.Ckatt;
+
 public class AddingGroups {
 
 	public AddingGroups() {
@@ -17,20 +19,38 @@ public class AddingGroups {
 
 		Collection<Integer> collection = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
 
+		for (Integer c : collection) {
+			Ckatt.outConsole(c);
+		}
+		
+		
+		
 		Integer[] moreInts = { 6, 7, 8, 9, 10 };
-
 		collection.addAll(Arrays.asList(moreInts));
 
+		
+		
+		
+		
 		// Работает значительно быстрее
 		// но Collections так конструировать не возможно :
 		Collections.addAll(collection, 11, 12, 13, 14, 15);
 		Collections.addAll(collection, moreInts);
+		
+		
 
 		// Создает список на базе массива
 
 		List<Integer> list = Arrays.asList(16, 17, 18, 19, 20);
 		list.set(1, 99);  // OK -- изменить елемент
-		list.add(21);     // Ошибка времени выполнения 
+		
+		for (Integer c : list) {
+			Ckatt.outConsole(c);
+		}
+		
+		
+		
+		//list.add(21);     // Ошибка времени выполнения 
 		                  // размер базового масива изменяться не может
  
 	}
